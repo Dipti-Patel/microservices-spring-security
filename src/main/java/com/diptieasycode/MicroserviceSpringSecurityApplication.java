03e90fec5832a9e261bb910971d855a3cbed777d
dipti.patel <dipti@gmail.com>
2022-10-03 10:45:08 +0530

    initial commit

---
+++ src/main/java/com/diptieasycode/MicroserviceSpringSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.diptieasycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSpringSecurityApplication.class, args);
+		System.out.println("hi");
+	}
+
+}
---
+++ src/test/java/com/diptieasycode/microservicespringsecurity/MicroserviceSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diptieasycode.microservicespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
